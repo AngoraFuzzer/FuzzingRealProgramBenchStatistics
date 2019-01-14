35ab5de20e30f606ddd31465dc537d8d4479c2ca
sp1npx <spinpx@gmail.com>
2019-01-14 16:32:27 +0800

    sort by number

--- stat.sh
+++ stat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat Readme.md| awk ' BEGIN { RS = "\n" ; FS = "," } /^\-/  {tot[$1]++}  END {for (i in tot) print tot[i],i} ' | sort -r  > rank.txt
+cat Readme.md| awk ' BEGIN { RS = "\n" ; FS = "," } /^\-/  {tot[$1]++}  END {for (i in tot) print tot[i],i} ' | sort -r -n  > rank.txt
